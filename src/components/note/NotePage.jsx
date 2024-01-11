b7e29db33baaa7426fa90fcdfdc0e0f5f7001ce3
Clay21c <Clay21c99@gmail.com>
2024-01-11 11:21:08 -0500

    components

--- src/components/note/NotePage.jsx
+++ src/components/note/NotePage.jsx
@@ -4,6 +4,22 @@ const NotePage = () => {
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
+      <div className='notes__content'>
+        <input
+          type='text'
+          placeholder='awesome title '
+          className='notes__title-input'
+        />
+        <textarea
+          placeholder='what happend today'
+          className='notes__textarea'></textarea>
+      </div>
+      <div className='notes__image'>
+        <img
+          src='https://pics.craiyon.com/2023-07-13/d617db6812624347adf3fe329013ee6a.webp'
+          alt='anime girl'
+        />
+      </div>
     </div>
   );
 };
